2af6223fbcbb6f1743f7b1075c8059784a3143f9
윤지 <shinyunjy412@naver.com>
2026-04-24 11:26:48 +0900

    1차 개발 진행중

--- src/main/java/org/example/demo2/Demo2Application.java
+++ src/main/java/org/example/demo2/Demo2Application.java
@@ -20,6 +20,9 @@ public class Demo2Application {
     public String getStatus() {
         return "Application is running";
     }
+    public String getStatus1() {
+        return "Application is running";
+    }
 
 
 }
